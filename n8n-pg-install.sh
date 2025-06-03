ba29e13976d8f135f7cd388fbd40373cc31f2644
rarebox <info@rarebox.net>
2025-06-03 21:23:40 +0300

    update script

--- n8n-pg-install.sh
+++ n8n-pg-install.sh
@@ -10,7 +10,22 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y curl wget git
 
 # Docker kurulumu
-sudo apt install -y docker.io
+# Docker için resmi repo kurulumu
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+
+# Docker CE kur
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
 sudo systemctl start docker
 sudo systemctl enable docker
 
